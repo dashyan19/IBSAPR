e5ab3c600914df1208e8fbad0bcf8a61130bd1ce
Дашаа <104989510+dashyan19@users.noreply.github.com>
2023-10-16 22:08:15 +0300

    LAB1-2
    lab1-2

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(229, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Шифрование";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 122);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 40);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Отчистить поля";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 122);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 40);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Загрузить из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 353);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 40);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "ШИФРОВАТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 428);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(513, 22);
+            this.textBox3.TabIndex = 20;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 408);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Шифротекст";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 307);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(513, 22);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Открытый текст";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 539);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(513, 22);
+            this.textBox2.TabIndex = 26;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(428, 519);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Гамма-КЛЮЧ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(190, 122);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 40);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "Загрузить в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 13);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(509, 40);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "Информация";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(190, 178);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 40);
+            this.button6.TabIndex = 29;
+            this.button6.Text = "Открыть файл";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(15, 178);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 40);
+            this.button7.TabIndex = 30;
+            this.button7.Text = "Открыть файл";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 584);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+
+        string russianAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789 .,:;!?-"; // Русский алфавит, цифры, пробел, знаки препинания
+        int countrussianAlphabet = 51; // Количесвто символов в алфавите
+
+        string text1 = "";
+        string text2 = "";
+
+        int characterCount1 = 0; // Переменная для хранения количества символов ОТКРЫТОГО ТЕКСТА
+
+        string resultat1 = "";
+        string resultat2 = "";
+
+        StringBuilder result = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.Enabled = false; // Заблокировать редактирование textBox3 (шифрование)
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            textBox3.Text = "";
+            textBox3.Clear();
+
+            text1 = "";
+
+            int maxLength = 50;
+            if (textBox1.Text.Length > maxLength)
+            {
+                textBox1.Text = textBox1.Text.Substring(0, maxLength);
+                textBox1.SelectionStart = maxLength;
+            }
+
+            string inputText = textBox1.Text.ToUpper(); // Преобразуем к верхнему регистру
+
+            // Подсчитываем количество символов
+            characterCount1 = textBox1.Text.Length;
+
+            StringBuilder resultText = new StringBuilder();
+
+            foreach (char character in inputText)
+            {
+                int position = russianAlphabet.IndexOf(character);
+
+                if (position != -1) // если символ найден в алфавите, то...
+                {
+                    // Номер символа по счёту (начиная с 1)
+                    resultText.Append(position.ToString() + " ");
+                }
+                else // если символ не найден в алфавите, то...
+                {
+                    inputText = inputText.Substring(0, inputText.Length - 1);
+                    textBox1.Text = inputText;
+                    textBox1.SelectionStart = textBox1.Text.Length; // Переместить курсор в конец текста
+                }
+            }
+
+            text1 = resultText.ToString().Trim();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            textBox3.Text = "";
+            textBox3.Clear();
+
+            text2 = "";
+
+            int maxLength = 50;
+            if (textBox2.Text.Length > maxLength)
+            {
+                textBox2.Text = textBox2.Text.Substring(0, maxLength);
+                textBox2.SelectionStart = maxLength;
+            }
+
+            string inputText = textBox2.Text.ToUpper(); // Преобразуем к верхнему регистру
+            string inputText0 = "";
+
+            inputText0 = inputText;
+
+            while (inputText0.Length < characterCount1)
+            {
+                inputText0 += inputText0; // Удваиваем строку
+            }
+
+            // Если строка превысила желаемую длину, обрезаем ее
+            if (inputText0.Length > characterCount1)
+            {
+                inputText0 = inputText0.Substring(0, characterCount1);
+            }
+
+            inputText = inputText0;
+
+            StringBuilder resultText = new StringBuilder();
+
+            foreach (char character in inputText)
+            {
+                int position = russianAlphabet.IndexOf(character);
+
+                if (position != -1) // если символ найден в алфавите, то...
+                {
+                    // Номер символа по счёту (начиная с 1)
+                    resultText.Append(position.ToString() + " ");
+                }
+                else // если символ не найден в алфавите, то...
+                {
+                    inputText = inputText.Substring(0, inputText.Length - 1);
+                    textBox2.Text = inputText;
+                    textBox2.SelectionStart = textBox2.Text.Length; // Переместить курсор в конец текста
+                }
+            }
+
+            text2 = resultText.ToString().Trim();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Результат: " + text1);
+            //MessageBox.Show("Результат: " + text2);
+            //MessageBox.Show("Количество символов: " + characterCount1.ToString());
+
+            string[] posl1 = text1.Split(' ');
+            string[] posl2 = text2.Split(' ');
+
+            if ((!string.IsNullOrWhiteSpace(textBox1.Text)) && (!string.IsNullOrWhiteSpace(textBox2.Text)))
+            {
+
+                for (int i = 0; i < posl1.Length; i++)
+                {
+                    int posled1 = int.Parse(posl1[i]);
+                    int posled2 = int.Parse(posl2[i]);
+                    int сумма = posled1 ^ posled2;
+                    int остаток = сумма % countrussianAlphabet;
+                    if (остаток == сумма)
+                    {
+                        resultat1 += сумма.ToString() + " ";
+                    }
+                    else if (остаток != сумма)
+                    {
+                        resultat1 += остаток.ToString() + " ";
+                    }
+                }
+
+                // Удаление лишнего пробела в конце строки
+                resultat1 = resultat1.Trim();
+
+                string[] numbers1 = resultat1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string numberStr in numbers1)
+                {
+                    if (int.TryParse(numberStr, out int number))
+                    {
+                        char russianChar = russianAlphabet[number];
+                        resultat2 += russianChar;
+                    }
+                    else
+                    {
+                        resultat2 += "*"; // Здесь можно установить другой символ по вашему выбору для некорректных чисел
+                    }
+                }
+                textBox3.Text = resultat2;
+            }
+
+            else if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Пожалуйста, введите открытый текст");
+            }
+
+            else if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Пожалуйста, введите гамма-ключ");
+            }
+
+            characterCount1 = 0;
+            resultat1 = "";
+            resultat2 = "";
+            //MessageBox.Show("Результат: " + text1);
+            //MessageBox.Show("Результат: " + text2);
+            //MessageBox.Show("Количество символов: " + characterCount.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int maxCharacters = 50;
+
+            string filePath = "C:/Users/NDA/Desktop/5sem/Информационная безопасность САПР - з/lab/lab1/lab1/input.txt"; // указать путь к файлу
+            string[] lines = File.ReadAllLines(filePath); // считываем все строки из файла
+
+            if (lines.Length >= 2)
+            {
+                bool hasInvalidChars = false; // флаг для определения наличия невалидных символов
+
+                // Проверяем каждую строку на наличие невалидных символов
+                foreach (var line in lines)
+                {
+                    foreach (char c in line)
+                    {
+                        if (!russianAlphabet.Contains(c))
+                        {
+                            hasInvalidChars = true;
+                            break;
+                        }
+                    }
+
+                    if (hasInvalidChars)
+                    {
+                        break;
+                    }
+                }
+
+                if (hasInvalidChars)
+                {
+                    MessageBox.Show("Обнаружены невалидные символы в файле.", "Ошибка");
+                    textBox1.Text = string.Empty; // очищаем textBox1
+                    textBox2.Text = string.Empty; // очищаем textBox2
+                }
+                else
+                {
+                    if (lines[0].Length > maxCharacters && lines[1].Length > maxCharacters)
+                    {
+                        MessageBox.Show("Ошибка: Исходный текст и гамма в файле превышают максимальное количество символов!");
+                    }
+                    else if (lines[0].Length > maxCharacters)
+                    {
+                        MessageBox.Show("Ошибка: Исходный текст в файле превышает максимальное количество символов!");
+                    }
+                    else if (lines[1].Length > maxCharacters)
+                    {
+                        MessageBox.Show("Ошибка: Гамма в файле превышает максимальное количество символов!");
+                    }
+                    else
+                    {
+                        // Отображаем данные в textBox1 и textBox2
+                        textBox1.Text = lines[0];
+                        textBox2.Text = lines[1];
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("В файле не хватает двух строк.", "Ошибка");
+                textBox1.Text = string.Empty; // очищаем textBox1
+                textBox2.Text = string.Empty; // очищаем textBox2
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Clear();
+            textBox2.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox3.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text01 = textBox1.Text;
+            string text02 = textBox2.Text;
+
+            if (string.IsNullOrEmpty(text01) || string.IsNullOrEmpty(text02))
+            {
+                MessageBox.Show("Пожалуйста, заполните оба текстовых поля перед сохранением.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Получаем текст из TextBox и добавляем пояснения
+                    string textToSave = $"Открытый текст: {textBox1.Text}\r\nГамма: {textBox2.Text}\r\nРезультат шифрования: {textBox3.Text}";
+
+                    // Задаем путь к файлу
+                    string filePath = "C:/Users/NDA/Desktop/5sem/Информационная безопасность САПР - з/lab/lab1/lab1/output.txt";
+
+                    // Записываем текст в файл
+                    File.WriteAllText(filePath, textToSave);
+
+                    MessageBox.Show("Данные успешно сохранены в файл.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string filePath = "C:/Users/NDA/Desktop/5sem/Информационная безопасность САПР - з/lab/lab1/lab1/info.txt";
+
+            try
+            {
+                // Проверяем, существует ли файл по указанному пути
+                if (File.Exists(filePath))
+                {
+                    // Открываем файл в блокноте (или другом текстовом редакторе)
+                    System.Diagnostics.Process.Start("notepad.exe", filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string filePath = "C:/Users/NDA/Desktop/5sem/Информационная безопасность САПР - з/lab/lab1/lab1/output.txt";
+
+            try
+            {
+                // Проверяем, существует ли файл по указанному пути
+                if (File.Exists(filePath))
+                {
+                    // Открываем файл в блокноте (или другом текстовом редакторе)
+                    System.Diagnostics.Process.Start("notepad.exe", filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string filePath = "C:/Users/NDA/Desktop/5sem/Информационная безопасность САПР - з/lab/lab1/lab1/input.txt";
+
+            try
+            {
+                // Проверяем, существует ли файл по указанному пути
+                if (File.Exists(filePath))
+                {
+                    // Открываем файл в блокноте (или другом текстовом редакторе)
+                    System.Diagnostics.Process.Start("notepad.exe", filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
